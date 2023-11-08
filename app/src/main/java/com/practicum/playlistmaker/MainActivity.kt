747f5320becc97cc94efe52f3629c1b95bc81414
Spyker3D <134065294+Spyker3D@users.noreply.github.com>
2023-11-08 20:48:10 +0300

    PlayList Maker project (Sprint 8) (#1)
    * First commit. Play Maker project
    
    * First commit. Play Maker project
    
    * First commit. Play Maker project
    
    * New fonts added
    
    * Sprint 8 task: comments cleared
    
    * Sprint 8 task: final version after revision
    
    * Sprint 8 task: final version after revision

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.buttonSearch)
+        val mediaLibraryButton = findViewById<Button>(R.id.buttonMediaLibrary)
+        val settingsButton = findViewById<Button>(R.id.buttonSettings)
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        mediaLibraryButton.setOnClickListener {
+            val mediaLibraryIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(mediaLibraryIntent)
+        }
+
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
